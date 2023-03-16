1cb34b523cbada368bd1e3da615f149e85e1d5a7
Pavluzont <ptsitou@exadel.com>
2023-03-16 15:15:22 +0100

    feat: add ex4 solution

--- section2_classes_objects/ex2, 3, 4/ArrayDemo/src/ArrayDemo.java
+++ section2_classes_objects/ex2, 3, 4/ArrayDemo/src/ArrayDemo.java
@@ -5,5 +5,7 @@ public class ArrayDemo {
         System.out.println(days[3]);
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(nums);
+        char[][] ticTacToe = {{'X','O','O'}, {'O','O','X'}, {'O','X','X'}};
+        System.out.println(ticTacToe[2][2]);
     }
 }
