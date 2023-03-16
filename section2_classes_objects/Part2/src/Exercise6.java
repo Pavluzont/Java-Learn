bc7268a7ed0fdfe16b0e6bd970c217680b12d19b
Pavluzont <ptsitou@exadel.com>
2023-03-16 15:29:02 +0100

    feat: add ex6 solution

---
+++ section2_classes_objects/Part2/src/Exercise6.java
@@ -0,0 +1,9 @@
+public class Exercise6 {
+    private static void specialMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main(String[] args) {
+        Exercise6.specialMethod();
+    }
+}
