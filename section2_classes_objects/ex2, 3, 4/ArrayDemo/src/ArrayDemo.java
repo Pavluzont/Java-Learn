260d05afd5636e8398f08f93b1a316aed58cee38
Pavluzont <ptsitou@exadel.com>
2023-03-16 15:07:46 +0100

    feat: add ex3 solution

--- section2_classes_objects/ex2, 3, 4/ArrayDemo/src/ArrayDemo.java
+++ section2_classes_objects/ex2, 3, 4/ArrayDemo/src/ArrayDemo.java
@@ -3,5 +3,7 @@ public class ArrayDemo {
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         System.out.println(days.length);
         System.out.println(days[3]);
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(nums);
     }
 }
