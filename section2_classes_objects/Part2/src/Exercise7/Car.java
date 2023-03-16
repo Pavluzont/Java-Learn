a2a4e86e844b210a46790cef775547769036a3f1
Pavluzont <ptsitou@exadel.com>
2023-03-16 16:14:24 +0100

    feat: add ex7 solution

---
+++ section2_classes_objects/Part2/src/Exercise7/Car.java
@@ -0,0 +1,23 @@
+package Exercise7;
+
+public class Car {
+    private String brand;
+    private String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String toString() {
+        return "Car{" +
+                ", brand='" + brand + '\'' +
+                ", model=" + model +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Car newCar = new Car("Nissan", "R34 Skyline");
+        System.out.println(newCar);
+    }
+}
