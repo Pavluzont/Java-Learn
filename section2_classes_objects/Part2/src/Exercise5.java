d6c3030ac211fd23ab75c82141f714733cf6693b
Pavluzont <ptsitou@exadel.com>
2023-03-16 15:38:16 +0100

    feat: add ex5 solution

---
+++ section2_classes_objects/Part2/src/Exercise5.java
@@ -0,0 +1,10 @@
+public class Exercise5 {
+    private void addMoreStrings(String...args) {
+
+    }
+    public static void main(String[] args) {
+        Exercise5 exercise = new Exercise5();
+        exercise.addMoreStrings("dog", "cat", "mouse");
+        exercise.addMoreStrings("corgi", "whiskey");
+    }
+}
