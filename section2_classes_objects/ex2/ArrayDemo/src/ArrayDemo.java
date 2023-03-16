a908571fe567d463be8235bd879261259c1428b3
Pavluzont <ptsitou@exadel.com>
2023-03-16 15:01:53 +0100

    feat: add ex2 solution

---
+++ section2_classes_objects/ex2/ArrayDemo/src/ArrayDemo.java
@@ -0,0 +1,7 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println(days.length);
+        System.out.println(days[3]);
+    }
+}
