0c62a266d70a660332a2a52f1e7de1e86945decb
Pavluzont <ptsitou@exadel.com>
2023-03-16 14:40:40 +0100

    feat:add ex1 solution

---
+++ section2_classes_objects/ex1/Business/src/business/Company.java
@@ -0,0 +1,12 @@
+package business;
+
+public class Company {
+    FactoryWorker worker;
+
+    public static void main(String[] args) {
+        FactoryWorker worker = new FactoryWorker("Oliver", 3);
+        worker.sayHello();
+        char initial = worker.getFirstNameInitial();
+        System.out.println(initial);
+    }
+}
