c72931e971877a91f5a6cc43a51e76473b05343e
Pavluzont <ptsitou@exadel.com>
2023-03-17 12:17:38 +0100

    feat: add ex10 solution

---
+++ section2_classes_objects/Part2/src/Exercise10/Mathematics.java
@@ -0,0 +1,9 @@
+package Exercise10;
+
+public class Mathematics {
+    public static final double numberE = Math.E;
+
+    public static void main(String[] args) {
+        System.out.println(numberE);
+    }
+}
--- section2_classes_objects/Part2/src/Exercise9/Customer.java
+++ section2_classes_objects/Part2/src/Exercise9/Customer.java
@@ -1,2 +1,18 @@
-package Exercise9;public class Customer {
+package Exercise9;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private float deposit;
+
+    public Customer(String firstName, String lastName, float deposit) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.deposit = deposit;
+    }
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("Agent", "Smith", 1408);
+        System.out.println(customer);
+    }
 }
