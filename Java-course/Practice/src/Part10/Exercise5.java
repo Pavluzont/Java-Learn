65117100b3ed9b40a520a38e34309c1b1037a44f
Pavluzont <ptsitou@exadel.com>
2023-04-07 17:57:17 +0200

    feat: add ex5 solution

---
+++ Java-course/Practice/src/Part10/Exercise5.java
@@ -0,0 +1,28 @@
+package Part10;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+
+public class Exercise5 {
+    record Car(String brand, String model, Year year) {}
+
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("Nissan", "Skyline", Year.of(1993)));
+        cars.add(new Car("Toyota", "Supra", Year.of(1989)));
+        cars.add(new Car("BMW", "M3", Year.of(2003)));
+        cars.add(new Car("Dodge", "Charger", Year.of(1963)));
+        cars.add(new Car("Kia", "Rio", Year.of(2014)));
+
+        cars.stream()
+                .sorted(
+                        Comparator.comparing(Car::brand).
+                                thenComparing(Car::model).reversed().
+                                thenComparing(Car::year)
+                )
+                .forEach(System.out::println);
+    }
+}
