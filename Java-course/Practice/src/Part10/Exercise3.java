31e13a9b68d381589c45b8430eb30a3c41ff0900
Pavluzont <ptsitou@exadel.com>
2023-04-07 17:22:21 +0200

    feat: add ex4 solution

--- Java-course/Practice/src/Part10/Exercise3.java
+++ Java-course/Practice/src/Part10/Exercise3.java
@@ -1,15 +1,17 @@
 package Part10;
 
 import java.time.Year;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Exercise3 {
+    record Car(String brand, String model, Year year) {}
     public static void main(String[] args) {
-        List<Exercise2.Car> cars = new ArrayList<>();
-        record Car(String brand, String model, Year year) {}
-        Stream.of(new Exercise2.Car("Nissan", "Skyline", Year.of(1993)), new Exercise2.Car("Toyota", "Supra", Year.of(1989)))
+        Stream.of(
+                new Car("Nissan", "Skyline", Year.of(1993)),
+                        new Car("Toyota", "Supra", Year.of(1989)),
+                        new Car("BMW", "M3", Year.of(2003)),
+                        new Car("Dodge", "Charger", Year.of(1963)),
+                        new Car("Kia", "Rio", Year.of(2014)))
                 .forEach(System.out::println);
     }
 }
